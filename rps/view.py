ab728b5566087e9c80d50f12f23ef2222586987f
baislsl <baislsl666@gmail.com>
2017-12-08 22:10:38 +0800

    记录保存和查看

--- rps/view.py
+++ rps/view.py
@@ -2,6 +2,9 @@ from django.http import HttpResponse
 from django.template import loader
 from .control.game import Game
 from django.shortcuts import render
+from .models import Record
+from django.utils import timezone
+import time
 
 games = {}
 
@@ -25,6 +28,23 @@ def play(request):
 def handon(request):
     id = request.POST['id']
     action = request.POST.getlist('action')
-    context = {}
 
+    record = Record()
+    record.user_id = str(id)
+    record.user_action = int(action[0])
+    record.competitor_id = "competitor-" + str(id)
+    record.competitor_action = (record.user_action + 1) % 3
+    record.date = timezone.now()
+
+    old_record_list = Record.objects.order_by('-date')
+
+    if len(old_record_list) == 0:
+        record.count = 1
+    else:
+        record.count = old_record_list[0].count + 1
+
+    record.save()
+
+    record_list = Record.objects.filter(user_id=id).order_by('-date')[:10]
+    context = {'history_list': record_list}
     return render(request, 'rps/play.html', context)
