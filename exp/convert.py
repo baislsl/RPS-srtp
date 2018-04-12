5601a2b6b19aee6b218402a769385869a2b12d58
baislsl <baislsl666@gmail.com>
2018-04-12 23:35:37 +0800

    convert to csv

--- exp/convert.py
+++ exp/convert.py
@@ -1,8 +1,7 @@
 import sqlite3
-
+import csv
 
 """
-
 CREATE TABLE "rps_record" (
     "id" integer NOT NULL PRIMARY KEY AUTOINCREMENT, 
     "id1" varchar(200) NOT NULL, 
@@ -13,7 +12,6 @@ CREATE TABLE "rps_record" (
     "count" integer NOT NULL, 
     "date" datetime NOT NULL
 );
-
 """
 
 sql_query = 'SELECT id1, id2, action1, action2, count, date FROM rps_record'
@@ -24,11 +22,18 @@ cursor = connect.cursor()
 
 result = cursor.execute(sql_query)
 
-for row in result:
-    print(row[4], '|', 'id1=', row[0], ',id2=', row[1], ',action1=', row[2], ',action2=', row[3])
+#
+# for row in result:
+#     print(row[4], '|', 'id1=', row[0], ',id2=', row[1], ',action1=', row[2], ',action2=', row[3])
 
+headers = ['count', 'id1', 'id2', 'action1', 'action2']
 
-
-
-
-
+rows = []
+with open(path + '.csv', 'w') as csv_file:
+    f_csv = csv.writer(csv_file)
+    f_csv.writerow(headers)
+    for row in result:
+        data = [row[4], row[0], row[1], row[2], row[3]]
+        rows.append(data)
+    print(rows)
+    f_csv.writerows(rows)
